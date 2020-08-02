0f29601ab8289d31cbf739dc8f435f16cf77c53b
Judy <kimjudyh@gmail.com>
2020-08-02 00:35:27 -0700

    created base stats model

--- pvp_app/models.py
+++ pvp_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class BaseStats(models.Model):
+    p_id = models.CharField(max_length=25)
+    species = models.CharField(max_length=100)
+    hp = models.IntegerField()
+    attack = models.IntegerField()
+    defense = models.IntegerField()
+
+
+    def __str__(self):
+        return self.species
