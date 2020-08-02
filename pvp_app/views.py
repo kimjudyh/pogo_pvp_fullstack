9d4fb754268fe98fa6488a978eddcc8e3e08d4b5
Judy <kimjudyh@gmail.com>
2020-08-01 23:15:47 -0700

    created new django project and app

---
+++ pvp_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse('<h1>hello world</h1>')
+
