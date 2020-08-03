a84ca09fe7942ebb056c88ec840adf5a521e6c2b
Judy <kimjudyh@gmail.com>
2020-08-03 00:56:42 -0700

    added multiple entry analysis and display for pvp stats

--- pvp_app/views.py
+++ pvp_app/views.py
@@ -13,21 +13,41 @@ def analyze(request):
     if request.method == 'POST':
         print(request.POST)
 
-        req_pokemon = request.POST['pokemon']
-        attack = request.POST['attack']
-        defense = request.POST['defense']
-        stamina = request.POST['stamina']
+        # deal with multiple entries
 
+        req_pokemon = request.POST['pokemon']
         pokemon = LeagueStats(req_pokemon.lower())
-        stats = pokemon.get_stat_product('GL', attack, defense, stamina)
+        stats_array = []
+
+        # need to use getlist to save all values, otherwise will default to giving just last value
+        a = request.POST.getlist('attack')
+        d = request.POST.getlist('defense')
+        s = request.POST.getlist('stamina')
+        print(a, d, s)
+
+        # multiple entries: 'attack': [1, 3, 6] etc.
+        for attack, defense, stamina in zip(a, d, s):
+            stats = pokemon.get_stat_product('GL', attack, defense, stamina)
+            stats['attack'] = attack
+            stats['defense'] = defense
+            stats['stamina'] = stamina
+            stats_array.append(stats)
+            print(stats_array)
+
+
+        # attack = request.POST['attack']
+        # defense = request.POST['defense']
+        # stamina = request.POST['stamina']
+
+        # stats = pokemon.get_stat_product('GL', attack, defense, stamina)
 
         template = 'home.html'
         context = {
             'pokemon': req_pokemon,
-            'attack': attack,
-            'defense': defense,
-            'stamina': stamina,
-            'stats': stats}
+            'attack': request.POST['attack'],
+            'defense': request.POST['defense'],
+            'stamina': request.POST['stamina'],
+            'stats_array': stats_array}
         return render(request, template, context)
         # return JsonResponse(stats)
 
