af4c3a59797811b75cfa1c1bef15b81bb594b8c8
Judy <kimjudyh@gmail.com>
2020-08-03 12:54:52 -0700

    refined endless form behavior on button press and tab press

--- pvp_app/static/app.js
+++ pvp_app/static/app.js
@@ -32,8 +32,24 @@ add_button.addEventListener('click', (event) => {
   const new_row = document.createElement('div');
   let IV_row = document.querySelector('.IVs');
   new_row.innerHTML = IV_row.innerHTML;
+
+  let elems = new_row.getElementsByTagName('input');
+  let atk_input = undefined;
+  
+  for (let i = 0; i < elems.length; i++) {
+    if (elems[i].id == 'attack') {
+      atk_input = elems[i]
+    }
+
+    console.log(elems[i])
+    // elems[i].value = '';
+    // elems[i].defaultValue = '';
+    elems[i].removeAttribute('value');
+  }
+
   new_row.classList.add('IVs');
   input_rows.appendChild(new_row);
+  atk_input.focus();
 })
 
 // event listener to add new row on tab press
@@ -41,12 +57,23 @@ let input_rows = document.querySelector('.IVInputsContainer');
 
 input_rows.addEventListener('keydown', (event) => {
   console.log(event)
-  if (event.key == 'Tab' && event.target.id == 'stamina') {
+  // don't perform action if shift + tab pressed
+  // make new row if tab pressed, input is stamina field, and it's the last row of inputs in the form
+  if (event.key == 'Tab' && event.shiftKey == false && event.target.id == 'stamina' && event.target.parentElement.nextElementSibling == null) {
     console.log('pressed Tab')
     let input_rows = document.querySelector('.IVInputsContainer');
     const new_row = document.createElement('div');
     let IV_row = document.querySelector('.IVs');
     new_row.innerHTML = IV_row.innerHTML;
+
+    let elems = new_row.getElementsByTagName('input');
+
+    for (let i = 0; i < elems.length; i++) {
+      console.log(elems[i])
+      elems[i].removeAttribute('value');
+    }
+
+
     new_row.classList.add('IVs');
     input_rows.appendChild(new_row);
   }
